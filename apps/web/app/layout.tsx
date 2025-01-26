2ed4cc370acc4b15fa126d44d7e741480451c049
“Prajanya-Sharma” <prajanyasharma.bt23ele@pec.edu.in>
2025-01-26 23:18:01 +0530

    build sahi

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,9 +2,8 @@
 
 import { SessionProvider } from 'next-auth/react';  
 import localFont from 'next/font/local';
-import { Session } from 'next-auth';
 import './globals.css';
-import SignOutButton from '../components/signout';  
+import SignOutButton from '../components/signout';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -15,21 +14,17 @@ const geistMono = localFont({
   variable: '--font-geist-mono',
 });
 
-
 export default function RootLayout({
   children,
-  session, 
-}: Readonly<{
+}: {
   children: React.ReactNode;
-  session: Session; 
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-      
-        <SessionProvider session={session}>
-          <SignOutButton />  
-          {children} 
+        <SessionProvider>
+          <SignOutButton />
+          {children}
         </SessionProvider>
       </body>
     </html>
