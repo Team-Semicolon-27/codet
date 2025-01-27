45b05296ece9523fd75137eceea63a80acd76afc
mrinalgaur2005 <mrinalgaur30@gmail.com>
2025-01-27 17:00:06 +0530

    corrected the file structue only

--- prisma/prismatest.ts
+++ prisma/prismatest.ts
@@ -1,8 +1,8 @@
-import prisma from './prisma';
+import { db } from "../lib/db";
 
 async function main() {
   // Create a new user
-  const newUser = await prisma.user.create({
+  const newUser = await db.user.create({
     data: {
       email: 'test@example.com',
       name: 'Test User',
@@ -11,11 +11,11 @@ async function main() {
   console.log('Created new user:', newUser);
 
   // Fetch all users
-  const allUsers = await prisma.user.findMany();
+  const allUsers = await db.user.findMany();
   console.log('All users:', allUsers);
 
   // Clean up: delete the created user
-  await prisma.user.delete({
+  await db.user.delete({
     where: {
       id: newUser.id,
     },
@@ -27,5 +27,5 @@ main()
     console.error(e);
   })
   .finally(async () => {
-    await prisma.$disconnect();
+    await db.$disconnect();
   });
